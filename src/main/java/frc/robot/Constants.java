65613ffaeae0fd908f5c5fd4aaf9fae87bdcf18d
Nick P <nicolas.a.delapena@gmail.com>
2023-03-09 07:59:14 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,24 +18,19 @@ public class Constants {
     // drive control accelerations
     public static final double kThrottleAcceleration = 1;
     public static final double kRotationAcceleartion = 1;
+    
+    // divides operator input by this number in slow driving mode
+    public static final double kSlowDriveCoefficient = 2;
+
+    // controller ports
+    public static final int kP1XboxPort = 0;
+    public static final int kP2XboxPort = 0;
+    
+    // ADIS16470 IMU SPI Address
+    //public static final int kIMUSPIAddress = 0;
 
     // DIO ports
 
-
-    // gryo I2C addresses
-    public static final int kGyroAddress = 0x6b; // default I2C address for L3GD20H gyro
-    public static final int kRegOutX = 0x28; // register address for X-axis data
-    public static final int kRegOutY = 0x2A; // register address for Y-axis data
-    public static final int kRegOutZ = 0x2C; // register address for Z-axis data
-    public static final int kRegCtrl1 = 0x20; // register address for control register 1
-    public static final int kRegCtrl4 = 0x23; // register address for control register 4
-    
-    // controller port
-    public static final int kP1XboxPort = 0;
-    public static final int kP2XboxPort = 0;
-
-    // divides operator input by this number in slow driving mode
-    public static final double kSlowDriveCoefficient = 2;
 
     // this variable is actually not a constant 
     // it is being used to roughly tell how long its been in each phase of the game 
